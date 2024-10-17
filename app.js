f8d5f958321fc405910890ac16431a8054a6cdc2
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2024-10-17 05:35:45 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Parse incoming JSON requests
-
-// Example route
+// Basic route for testing
 app.get('/', (req, res) => {
   res.send('Welcome to BudgetEase API');
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
