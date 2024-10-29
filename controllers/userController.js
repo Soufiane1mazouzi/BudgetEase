95b2204302f3e8b4352136faada803f37b1ed596
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2024-10-29 13:29:39 +0000

    update Usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,15 @@
-const User = require('../models/user'); // Adjust path if necessary
+const User = require('../models/user');
 
-// Function for user registration
+// Function to handle user registration
 const registerUser = async (req, res) => {
     try {
-        const userData = req.body; // Get user data from request body
-        const newUser = await User.create({
-            name: userData.name, // Use the correct field names based on your User model
-            password: userData.password,
-        });
-        res.status(201).json({ message: 'User created successfully!', user: newUser });
+        const { username, password } = req.body;
+        const newUser = await User.create({ username, password });
+        res.status(201).json({ message: 'User created successfully', user: newUser });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error creating user', error });
     }
 };
 
-module.exports = { registerUser }; // Export the registerUser function
+module.exports = { registerUser };
