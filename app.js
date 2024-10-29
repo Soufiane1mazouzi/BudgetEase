e40b46c2ddda2c6639b3f546caa1445022386444
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2024-10-29 19:52:42 +0000

    correct console log

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-// Import modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/sequelize');
 
-// Initialize Express app
 const app = express();
 
-// Middleware to parse JSON
 app.use(bodyParser.json());
 
-// Import routes
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
 
-// Sync Sequelize models to initialize the database
 sequelize.sync()
     .then(() => console.log("Database & tables created!"))
     .catch(err => console.error("Error creating database:", err));
 
-// Start server on port 3000 and bind to '0.0.0.0'
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
