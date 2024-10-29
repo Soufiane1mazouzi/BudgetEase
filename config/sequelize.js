f33cca34b4e0fa70c1e81fe855157c5f9d5959e7
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2024-10-29 15:10:18 +0000

    sequelize update

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,7 +3,12 @@ const { Sequelize } = require('sequelize');
 // Initialize Sequelize with SQLite
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './database.sqlite'
+    storage: './database.sqlite', // Stores the database in the project root
 });
+
+// Test the connection to confirm it's working
+sequelize.authenticate()
+    .then(() => console.log("Connected to SQLite database"))
+    .catch(err => console.error("Unable to connect to database:", err));
 
 module.exports = sequelize;
